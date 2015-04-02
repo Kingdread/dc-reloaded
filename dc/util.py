1c0387bc0b70e276b6f91ac7b165b81cf0f27f67
Daniel <kingdread@gmx.de>
2015-04-02 23:55:23 +0200

    Added function to fix Qt's icon search path
    Looks like Qt has problems with system icons when you're not using KDE
    or Gnome (and don't talk about Windows, that's another story...). This
    fix currently works for Mate.

--- dc/util.py
+++ dc/util.py
@@ -3,6 +3,10 @@
 """
 Various utility functions and classes for DC
 """
+import ast
+import os
+import subprocess
+import sys
 
 
 def signed_value(val, bits):
@@ -40,6 +44,71 @@ def number_of_digits(value, base=10):
         return 1
     from math import log, floor
     return floor(log(value) / log(base)) + 1
+
+
+def get_desktop_environment():
+    """
+    Returns the currently running desktop environment. Detection currently
+    supports:
+    - Mate (returns "mate")
+    If detection failed, this function simply returns None
+    """
+    if sys.platform == "win32":
+        # One moment of silence for the poor souls who use windows
+        return
+
+    # Try to detect Mate
+    if "MATE_DESKTOP_SESSION_ID" in os.environ:
+        return "mate"
+    # MATE_DESKTOP_SESSION_ID is deprecated, so we might need a better
+    # detection since we can't guarantee that we're not running Mate:
+    command = [
+        "dbus-send", "--print-reply", "--dest=org.freedesktop.DBus",
+        "/org/freedesktop/DBus", "org.freedesktop.DBus.GetNameOwner",
+        "string:org.mate.SessionManager",
+    ]
+    try:
+        retval = subprocess.call(command, stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT)
+    except FileNotFoundError:
+        pass
+    else:
+        if retval == 0:
+            return "mate"
+
+
+def fix_qt_icon_theme():
+    """
+    Attempts to fix the icon theme and the icon theme search path in desktop
+    environments where Qt can't figure it out on its own. This is not
+    guaranteed to work for every desktop environment and might be a simple
+    no-op.
+    """
+    # Importing Qt here because we might use the module without a GUI. This way
+    # it will only error when we actually use fix_qt_icon_theme
+    from PyQt5 import QtGui
+
+    env_name = get_desktop_environment()
+    if env_name == "mate":
+        # We can get the icon theme name via dconf
+        command = ["dconf", "read", "/org/mate/desktop/interface/icon-theme"]
+        try:
+            icon_theme = subprocess.check_output(command)
+        except FileNotFoundError:
+            # Well, shit.
+            pass
+        except subprocess.CalledProcessError:
+            pass
+        else:
+            icon_theme = icon_theme.decode("ascii", "ignore").strip()
+            # looks like the output is enclosed in quotes
+            icon_theme = ast.literal_eval(icon_theme)
+            if not icon_theme:
+                return
+            paths = QtGui.QIcon.themeSearchPaths()
+            paths.append("/usr/share/icons")
+            QtGui.QIcon.setThemeSearchPaths(paths)
+            QtGui.QIcon.setThemeName(icon_theme)
 
 
 class IDict(dict):
